a152ef36f85d73f08211ce6c4c88d69c13841fb2
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-08-09 18:49:31 -0700

    index.js fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./app";
 import Container, { Global } from "./component/Container";
 
-if (process.env.NODE_ENV === "development") {
-  console.log("only run in dev mode");
-} else {
-  console.log("only runs in production");
-}
-
-render(<App />, document.getElementById("root"));
-
-export { Global };
-export default Container;
+export { Container, Global };
